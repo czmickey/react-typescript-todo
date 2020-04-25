39acbde2afa9f31e7b00bc20cfb578a2a822ae43
Michal Oktabec <michal.oktabec@lmc.eu>
2020-04-25 09:43:33 +0200

    Add basic functionality - render initial todos, add new todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
-function App() {
-  return <div>Hello</div>;
-}
+const initialTodos: Array<Todo> = [
+  { text: 'Walk the dog', complete: true },
+  { text: 'Go shopping', complete: false },
+];
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo: ToggleTodo = selectedTodo => {
+    const newTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
+  };
+
+  const addTodo: AddTodo = newTodo => {
+    newTodo.trim() !== '' &&
+      setTodos([...todos, { text: newTodo, complete: false }]);
+  };
+
+  return (
+    <React.Fragment>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <AddTodoForm addTodo={addTodo} />
+    </React.Fragment>
+  );
+};
 
 export default App;
